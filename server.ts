b9152d047ac2795edde4bbfe6a5a7ad0a4cf1ca1
Andi <andimilyas123@gmail.com>
2025-10-09 13:37:55 +0700

    chore: Update start script for server execution
    - Changed the start script to execute the server using `node server.ts` instead of the previous `next start` command.
    - Removed the HTTPS start script as it is no longer needed in the current setup.

---
+++ server.ts
@@ -0,0 +1,26 @@
+const { createServer } = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
+
+const dev = false;
+const hostname = '10.0.10.12';
+const port = 5002;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync('./ssl/server.key'),
+  cert: fs.readFileSync('./ssl/server.crt'),
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req: { url: any; }, res: any) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, hostname, (err: any) => {
+    if (err) throw err;
+    console.log(`> Ready on https://${hostname}:${port}`);
+  });
+});
